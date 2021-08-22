f315221c2847146d1623180858c4d59931a8c464
cgcdoss <gabi04061997@gmail.com>
2021-08-22 20:41:54 -0300

    init

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import http from 'http';
+import ws from 'ws';
+// import WebSocket, { WebSocketServer } from 'ws';
+
+// const app = express();
+// const httpServer = http.createServer(app);
+const server = new ws.Server({
+  port: 3333
+});
+
+let sockets: any[] = [];
+let messages: any[] = [];
+
+server.on('connection', (socket) => {
+  console.log('Um novo cliente conectou');
+  socket.send('Bem vindo ao socket');
+
+  // Adiciona cada nova conexão/socket ao array `sockets`
+  sockets.push(socket);
+
+  // Quando receber uma mensagem, envia ela para todos os sockets
+  socket.on('message', (msg) => {
+    console.log('Mensagem recebida:' + msg);
+    messages.push(msg);
+    sockets.forEach(s => s.send(msg.toString()));
+  });
+
+  // Quando a conexão de um socket é fechada/desconectada, remove o socket do array
+  socket.on('close', () => {
+    console.log('Um cliente se desconectou');
+    sockets = sockets.filter(s => s !== socket);
+  });
+});
+
+// httpServer.listen(3333);
+
+
+// cliente nodejs
+/* const app2 = express();
+app2.get('/t', async (req, res) => {
+  let client = new ws('ws://localhost:3333');
+  await new Promise(resolve => client.once('open', () => {
+    client.send(req.query.msg);
+    resolve('');
+  }));
+  await new Promise(resolve => client.on('message', (msg: any) => {
+    console.log('Msg no Cliente: ' + msg);
+    resolve(msg);
+    res.json(msg.toString());
+  }));
+
+  // Esperamos o cliente conectar com o servidor usando async/await
+
+  // res.json('ok');
+});
+
+app2.listen(3334);
+ */
